b34562fd342663eda171555121da8e70f04d132d
Benjamin Lucas <bnlcas@gmail.com>
2022-12-01 11:57:44 -0500

    initial sketch of editor function

--- js/image2video.js
+++ js/image2video.js
@@ -25,15 +25,114 @@ const image2video = async () => {
 const elm = document.getElementById('start-btn');
 elm.addEventListener('click', image2video);
 
+//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
+//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
+//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
+//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
+//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
 
-const example_video = document.getElementById("example_video");
-example_video.addEventListener('ended', (event) => {
-    setTimeout(() => {
-        example_video.currentTime = 0;
-        setTimeout(() => {
-            example_video.play();
-            //example_video.play();
-        }, 800);
-    }, 1000);
-});
+
+var totalfiles;
+function UploadFiles()
+{
+    var totalfiles = document.getElementById('files').files.length;
+
+}
+
+function DrawFrame(ind)
+{
+    canvas.width = images[ind].width;
+    canvas.height = images[ind].height;
+    ctx.drawImage(images[ind], 0, 0,images[ind].width, images[ind].height );
+}
+
+function UpdatePreview()
+{
+    frameNum = 0;
+    DrawFrame(frameNum);
+}
+
+var img_urls = [];
+var images = [];
+var AddImageFiles = async ({target: { files } }) => {
+    files = [].slice.call(files).sort(function(a, b){
+        if(a.name < b.name) { return -1; }
+        if(a.name > b.name) { return 1; }
+        return 0;
+    })
+
+    for(var i = 0; i < files.length; i++)
+    {   
+        let src_url = URL.createObjectURL(files[i]);
+        img_urls.push(src_url);
+        let img = new Image();
+        img.src = src_url;
+        images.push(img);
+    }
+    images[files.length - 1].onload = function() {
+        UpdatePreview();
+    };
+}
+
+
+
+function TogglePlay()
+{
+    isPlaying = !isPlaying;
+    if(isPlaying)
+    {
+        if(frameNum == img_urls.length - 1)
+        {
+            frameNum = 0;
+        }
+        setTimeout(AdvanceFrame, frameDuration);
+    }
+    else
+    {
+
+    }
+}
+
+function AdvanceFrame()
+{
+    if(isPlaying)
+    {
+        frameNum += 1;
+        if(frameNum >= img_urls.length)
+        {
+            if(loopClip)
+            {
+                frameNum = 0;
+            }
+            else
+            {
+                frameNum -= 1;
+                isPlaying = false;
+            }
+        }
+        DrawFrame(frameNum);
+        setTimeout(AdvanceFrame, frameDuration);
+    }
+}
+
+
+
+//document.getElementById('uploadButton').addEventListener('change', SetFileAddress);// transcode);
+
+//document.getElementById('uploadButton').addEventListener('onclick',UploadFiles );
+
+
+
+document.getElementById('image_uploader').addEventListener('change', AddImageFiles);
+
+
+const canvas = document.getElementById("previewCanvas");
+const ctx = canvas.getContext("2d");
+canvas.addEventListener("click", TogglePlay);
+
+var frameDuration = 500;
+var loopClip = false;
+
+var isPlaying = false;
+var frameNum = 0;
 
