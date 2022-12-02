c5b74fd8db7c6ad84533d8aa667a75bcead32a9f
Benjamin Lucas <bnlcas@gmail.com>
2022-12-01 19:59:14 -0500

    js/image2video.js

--- js/image2video.js
+++ js/image2video.js
@@ -1,38 +1,76 @@
+//
+//FFMPEG WASM Section
+//
 const { createFFmpeg, fetchFile } = FFmpeg;
 const ffmpeg = createFFmpeg({ log: true });
 
-const image2video = async () => {
-  const message = document.getElementById('message');
-  message.innerHTML = 'Loading ffmpeg-core.js';
-  await ffmpeg.load();
-  message.innerHTML = 'Loading data';
-  for (let i = 0; i < 60; i += 1) {
-    const num = `00${i}`.slice(-3);
-    ffmpeg.FS('writeFile', `tmp.${num}.png`, await fetchFile('./assets/triangle/tmp.${num}.png'));
+ const CreateVideo = async () => {
+    const message = document.getElementById('VideoProcessingMessage');
+    message.innerHTML = 'Loading ffmpeg-core.js';
+    await ffmpeg.load();
+    message.innerHTML = 'Loading data';
+
+    for (let i = 0; i < img_urls.length; i += 1) {
+        const frame_filename = 'tmp' + i.toString().padStart(4, '0') + ".png";
+        ffmpeg.FS('writeFile', frame_filename, await fetchFile(img_urls[i]));
+    }
+    
+    message.innerHTML = 'Start transcoding';
+    //await ffmpeg.run('-framerate', '10', '-pattern_type', 'glob', '-i', '*.png', 'copy', '-shortest', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', 'out.mp4');
+    await ffmpeg.run('-framerate', '30', '-pattern_type', 'glob', '-i', '*.png', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', 'out.mp4');
+    //await ffmpeg.run('-framerate', '30', '-pattern_type', 'glob', '-i', '*.png', 'out.mp4');
+    const data = ffmpeg.FS('readFile', 'out.mp4');
+
+    for (let i = 0; i < img_urls.length; i += 1) {
+        const frame_filename = 'tmp' + i.toString().padStart(4, '0') + ".png";
+        ffmpeg.FS('unlink', frame_filename);
+    }
+
+    let video_src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+
+
+    const a = document.createElement('a')
+    a.href = video_src
+    a.download = video_src.split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+    message.innerHTML = 'Done!';
   }
 
-  message.innerHTML = 'Start transcoding';
-  await ffmpeg.run('-framerate', '10', '-pattern_type', 'glob', '-i', '*.png', 'copy', '-shortest', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', 'out.mp4');
-  const data = ffmpeg.FS('readFile', 'out.mp4');
-  for (let i = 0; i < 60; i += 1) {
-    const num = `00${i}`.slice(-3);
-    ffmpeg.FS('unlink', `tmp.${num}.png`);
-  }
+const CreateGIF = async () =>{
+    const message = document.getElementById('VideoProcessingMessage');
+    message.innerHTML = 'Loading ffmpeg-core.js';
+    await ffmpeg.load();
+    message.innerHTML = 'Loading data';
 
-  const video = document.getElementById('output-video');
-  video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+    for (let i = 0; i < img_urls.length; i += 1) {
+      const frame_filename = 'tmp' + i.toString().padStart(4, '0') + ".png";
+      ffmpeg.FS('writeFile', frame_filename, await fetchFile(img_urls[i]));
+    }
+  
+    message.innerHTML = 'Start transcoding';
+    await ffmpeg.run('-framerate', '5', '-pattern_type', 'glob', '-i', '*.png', 'out.gif');
+    const data = ffmpeg.FS('readFile', 'out.gif');
+    
+    for (let i = 0; i < img_urls.length; i += 1) {
+        const frame_filename = 'tmp' + i.toString().padStart(4, '0') + ".png";
+        ffmpeg.FS('unlink', frame_filename);
+    }
+  
+    let gif_src = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+
+    const a = document.createElement('a')
+    a.href = gif_src
+    a.download = gif_src.split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+    message.innerHTML = 'Done!';
 }
-
-
-//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
-//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
-//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
-//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
-//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf//asdf
-
-
-
-
+//
+//GUI And All the Rest:
+//
 function DrawFrame(ind)
 {
     canvas.width = images[ind].width;
@@ -44,6 +82,13 @@ function UpdatePreview()
 {
     frameNum = 0;
     DrawFrame(frameNum);
+}
+
+function UpdateFrameDuration(e)
+{
+    frameDuration = 1000 * parseFloat(e.target.value);
+    e.target.labels[0].innerHTML = "Seconds Per Photo: " + (frameDuration/1000).toFixed(2).toString();
+     //console.log(frameDuration);});
 }
 
 function SortFiles(files)
@@ -65,6 +110,7 @@ function AddPlaybackControls()
     document.getElementById("add_button").style.visibility = 'visible';
     document.getElementById("add_button").style.pointerEvents = 'auto';
 
+    document.getElementById("upload_button").value="Upload A Different Set of Image Files"
 }
 
 function CreateImages(files)
@@ -98,7 +144,6 @@ var AddImageFiles = async ({target: { files } }) => {
     CreateImages(files);
 }
 
-
 function TogglePlay()
 {
     isPlaying = !isPlaying;
@@ -125,18 +170,13 @@ function AdvanceFrame()
             {
                 frameNum -= 1;
                 isPlaying = false;
+                document.getElementById("playButton").value = isPlaying ? 'Pause' : 'Play ';
             }
         }
         DrawFrame(frameNum);
         setTimeout(AdvanceFrame, frameDuration);
     }
 }
-
-
-
-//document.getElementById('uploadButton').addEventListener('change', SetFileAddress);// transcode);
-
-//document.getElementById('uploadButton').addEventListener('onclick',UploadFiles );
 
 const canvas = document.getElementById("previewCanvas");
 const ctx = canvas.getContext("2d");
@@ -153,21 +193,17 @@ var img_urls = [];
 var images = [];
 
 
-
 document.getElementById('upload_button').addEventListener('click', 
     () => document.getElementById('image_uploader').click());
 document.getElementById('image_uploader').addEventListener('change', UploadImageFiles);
-
-
 
 document.getElementById('add_button').addEventListener('click', 
     () => document.getElementById('image_appended').click());
 document.getElementById('image_appended').addEventListener('change', AddImageFiles);
 
-//document.getElementById("loopPlayback").addEventListener("change", ()=>{loopClip = document.getElementById("loopPlayback").checked;})
 document.getElementById("loopPlayback").addEventListener("change", (e)=>{loopClip = e.target.checked;});
-document.getElementById("loopPlayback").addEventListener("change", (e)=>{loopClip = e.target.checked;});
-document.getElementById("FrameTime").addEventListener("change", (e)=>{frameDuration = 1000 * parseFloat(e.target.value); console.log(frameDuration);});
+document.getElementById("FrameTime").addEventListener("input", UpdateFrameDuration);
 document.getElementById("playButton").addEventListener("click", TogglePlay);
 
-
+document.getElementById("exportButton").addEventListener("click", CreateVideo)
+document.getElementById("exportGIFButton").addEventListener("click", CreateGIF)
